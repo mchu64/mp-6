349d4b57806290e8835244d2bfcb8c172d06caa5
Michael Chu <mchu64@bu.edu>
2026-04-24 16:36:03 -0400

    .

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import Google from "next-auth/providers/google";
 
 const googleClientId = process.env.GOOGLE_CLIENT_ID;
 const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const authSecret = process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET;
+const authSecret = process.env.AUTH_SECRET;
 
 if (!googleClientId || !googleClientSecret) {
   throw new Error("Missing client ID or secret");
